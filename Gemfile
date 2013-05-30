2e240d18c981d28925bdd84e2ab4af1761a03ca2
Alvaro <alvaro@zauberlabs.com>
2013-05-30 15:51:17 -0300

    Finished sign up

--- Gemfile
+++ Gemfile
@@ -14,9 +14,14 @@ group :development do
   gem 'annotate', '2.5.0'
 end
 
+group :production do
+  gem 'pg', '0.15.1'
+end
+
 group :test do
   gem 'selenium-webdriver', '2.0.0'
   gem 'capybara', '2.1.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 gem 'sass-rails', '4.0.0.rc1'
@@ -33,6 +38,4 @@ group :doc do
   gem 'sdoc', '0.3.20', require: false
 end
 
-group :production do
-  gem 'pg', '0.15.1'
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
-  root 'static_pages#home'
-  
+
+  resources :users
+  root to: 'static_pages#home'
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
