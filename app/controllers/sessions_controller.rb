af51de6571f27be46e0c9191af6aacac8876c0e9
Alvaro <alvaro@zauberlabs.com>
2013-05-31 10:59:39 -0300

    Sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
 
+  #For providing a signin page
   def new
   end
 
+  # the authentication itself
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -15,6 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 
 end
